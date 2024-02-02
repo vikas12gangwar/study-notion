efe906905a09b25bd550afe71d2c144136a890a0
vikas12gangwar <vikasgangwarbly2003@gmail.com>
2024-02-02 23:02:30 +0530

    new commit in error

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -35,7 +35,7 @@ export function sendOtp(email, navigate) {
       navigate("/verify-email")
     } catch (error) {
       console.log("SENDOTP API ERROR............", error)
-      toast.error("Could Not Send OTP")
+      toast.error(`${error?.message}`)
     }
     dispatch(setLoading(false))
     toast.dismiss(toastId)
