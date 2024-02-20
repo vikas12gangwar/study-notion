120e325b1559c15fa3c3aa8199849b462e485c09
Vikas Gangwar <107347602+vikas12gangwar@users.noreply.github.com>
2024-02-20 23:50:35 +0530

    Update Section.js

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -1,13 +1,13 @@
 const Section = require("../models/Section")
 const Course = require("../models/Course")
 const SubSection = require("../models/Subsection")
-// CREATE a new section
+
 exports.createSection = async (req, res) => {
   try {
-    // Extract the required properties from the request body
+   
     const { sectionName, courseId } = req.body
 
-    // Validate the input
+  
     if (!sectionName || !courseId) {
       return res.status(400).json({
         success: false,
@@ -15,10 +15,10 @@ exports.createSection = async (req, res) => {
       })
     }
 
-    // Create a new section with the given name
+   
     const newSection = await Section.create({ sectionName })
 
-    // Add the new section to the course's content array
+   
     const updatedCourse = await Course.findByIdAndUpdate(
       courseId,
       {
@@ -36,14 +36,14 @@ exports.createSection = async (req, res) => {
       })
       .exec()
 
-    // Return the updated course object in the response
+  
     res.status(200).json({
       success: true,
       message: "Section created successfully",
       updatedCourse,
     })
   } catch (error) {
-    // Handle errors
+    
     res.status(500).json({
       success: false,
       message: "Internal server error",
@@ -52,7 +52,7 @@ exports.createSection = async (req, res) => {
   }
 }
 
-// UPDATE a section
+
 exports.updateSection = async (req, res) => {
   try {
     const { sectionName, sectionId, courseId } = req.body
@@ -85,7 +85,7 @@ exports.updateSection = async (req, res) => {
   }
 }
 
-// DELETE a section
+
 exports.deleteSection = async (req, res) => {
   try {
     const { sectionId, courseId } = req.body
@@ -102,12 +102,12 @@ exports.deleteSection = async (req, res) => {
         message: "Section not found",
       })
     }
-    // Delete the associated subsections
+   
     await SubSection.deleteMany({ _id: { $in: section.subSection } })
 
     await Section.findByIdAndDelete(sectionId)
 
-    // find the updated course and return it
+   
     const course = await Course.findById(courseId)
       .populate({
         path: "courseContent",
