5f44bc1f8b367335984460519e20daac3f480dc5
Vikas Gangwar <107347602+vikas12gangwar@users.noreply.github.com>
2024-02-20 23:54:29 +0530

    Update payments.js

--- server/controllers/payments.js
+++ server/controllers/payments.js
@@ -10,7 +10,7 @@ const {
 const { paymentSuccessEmail } = require("../mail/templates/paymentSuccessEmail")
 const CourseProgress = require("../models/CourseProgress")
 
-// Capture the payment and initiate the Razorpay order
+
 exports.capturePayment = async (req, res) => {
   const { courses } = req.body
   const userId = req.user.id
@@ -23,17 +23,17 @@ exports.capturePayment = async (req, res) => {
   for (const course_id of courses) {
     let course
     try {
-      // Find the course by its ID
+      
       course = await Course.findById(course_id)
 
-      // If the course is not found, return an error
+     
       if (!course) {
         return res
           .status(200)
           .json({ success: false, message: "Could not find the Course" })
       }
 
-      // Check if the user is already enrolled in the course
+     
       const uid = new mongoose.Types.ObjectId(userId)
       if (course.studentsEnroled.includes(uid)) {
         return res
@@ -41,7 +41,7 @@ exports.capturePayment = async (req, res) => {
           .json({ success: false, message: "Student is already Enrolled" })
       }
 
-      // Add the price of the course to the total amount
+      
       total_amount += course.price
     } catch (error) {
       console.log(error)
@@ -56,7 +56,7 @@ exports.capturePayment = async (req, res) => {
   }
 
   try {
-    // Initiate the payment using Razorpay
+    
     const paymentResponse = await instance.orders.create(options)
     console.log(paymentResponse)
     res.json({
@@ -71,7 +71,7 @@ exports.capturePayment = async (req, res) => {
   }
 }
 
-// verify the payment
+
 exports.verifyPayment = async (req, res) => {
   const razorpay_order_id = req.body?.razorpay_order_id
   const razorpay_payment_id = req.body?.razorpay_payment_id
@@ -105,7 +105,7 @@ exports.verifyPayment = async (req, res) => {
   return res.status(200).json({ success: false, message: "Payment Failed" })
 }
 
-// Send Payment Success Email
+
 exports.sendPaymentSuccessEmail = async (req, res) => {
   const { orderId, paymentId, amount } = req.body
 
@@ -138,7 +138,7 @@ exports.sendPaymentSuccessEmail = async (req, res) => {
   }
 }
 
-// enroll the student in the courses
+
 const enrollStudents = async (courses, userId, res) => {
   if (!courses || !userId) {
     return res
@@ -167,7 +167,7 @@ const enrollStudents = async (courses, userId, res) => {
         userId: userId,
         completedVideos: [],
       })
-      // Find the student and add the course to their list of enrolled courses
+     
       const enrolledStudent = await User.findByIdAndUpdate(
         userId,
         {
@@ -180,7 +180,7 @@ const enrollStudents = async (courses, userId, res) => {
       )
 
       console.log("Enrolled student: ", enrolledStudent)
-      // Send an email notification to the enrolled student
+    
       const emailResponse = await mailSender(
         enrolledStudent.email,
         `Successfully Enrolled into ${enrolledCourse.courseName}`,
