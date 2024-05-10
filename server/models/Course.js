f352319f5635f816706cd9f495729a4c6ef07d06
Vikas Gangwar <107347602+vikas12gangwar@users.noreply.github.com>
2024-05-10 14:16:41 +0530

    Update Course.js

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-// Define the Courses schema
+
 const coursesSchema = new mongoose.Schema({
   courseName: { type: String },
   courseDescription: { type: String },
@@ -56,5 +56,5 @@ const coursesSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 })
 
-// Export the Courses model
+
 module.exports = mongoose.model("Course", coursesSchema)
