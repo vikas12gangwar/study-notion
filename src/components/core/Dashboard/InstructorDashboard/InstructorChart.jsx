2fa401d725f96297bd9a8510790cfa3da7a53ce8
Vikas Gangwar <107347602+vikas12gangwar@users.noreply.github.com>
2024-05-10 14:56:26 +0530

    Update InstructorChart.jsx

--- src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
+++ src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
@@ -5,10 +5,8 @@ import { Pie } from "react-chartjs-2"
 Chart.register(...registerables)
 
 export default function InstructorChart({ courses }) {
-  // State to keep track of the currently selected chart
+  
   const [currChart, setCurrChart] = useState("students")
-
-  // Function to generate random colors for the chart
   const generateRandomColors = (numColors) => {
     const colors = []
     for (let i = 0; i < numColors; i++) {
@@ -20,7 +18,6 @@ export default function InstructorChart({ courses }) {
     return colors
   }
 
-  // Data for the chart displaying student information
   const chartDataStudents = {
     labels: courses.map((course) => course.courseName),
     datasets: [
@@ -31,7 +28,6 @@ export default function InstructorChart({ courses }) {
     ],
   }
 
-  // Data for the chart displaying income information
   const chartIncomeData = {
     labels: courses.map((course) => course.courseName),
     datasets: [
@@ -42,7 +38,7 @@ export default function InstructorChart({ courses }) {
     ],
   }
 
-  // Options for the chart
+  
   const options = {
     maintainAspectRatio: false,
   }
@@ -62,7 +58,7 @@ export default function InstructorChart({ courses }) {
         >
           Students
         </button>
-        {/* Button to switch to the "income" chart */}
+        
         <button
           onClick={() => setCurrChart("income")}
           className={`rounded-sm p-1 px-3 transition-all duration-200 ${
@@ -75,7 +71,7 @@ export default function InstructorChart({ courses }) {
         </button>
       </div>
       <div className="relative mx-auto aspect-square h-full w-full">
-        {/* Render the Pie chart based on the selected chart */}
+        
         <Pie
           data={currChart === "students" ? chartDataStudents : chartIncomeData}
           options={options}
